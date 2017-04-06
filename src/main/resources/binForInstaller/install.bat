b6fc5a11fa50be41ccaf9c1d91ee2797f2b7341f
sujkim <sujkim@datastreams.co.kr>
2017-04-06 20:15:17 +0900

    [update] resources

--- src/main/resources/binForInstaller/install.bat
+++ src/main/resources/binForInstaller/install.bat
@@ -15,4 +15,27 @@ popd
 popd
 popd
 
-java -cp "%PATH_TO_LIB_DIR%/*" install.Start -ask -install
+
+:init
+@rem Get command-line arguments, handling Windowz variants
+
+if not "%OS%" == "Windows_NT" goto win9xME_args
+if "%@eval[2+2]" == "4" goto 4NT_args
+
+:win9xME_args
+@rem Slurp the command line arguments.
+set CMD_LINE_ARGS=
+set _SKIP=2
+
+:win9xME_args_slurp
+if "x%~1" == "x" goto execute
+
+set CMD_LINE_ARGS=%*
+goto execute
+
+:4NT_args
+@rem Get arguments from the 4NT Shell from JP Software
+set CMD_LINE_ARGS=%$
+
+:execute
+java -cp "%PATH_TO_LIB_DIR%/*" install.Start -ask -install %CMD_LINE_ARGS%
