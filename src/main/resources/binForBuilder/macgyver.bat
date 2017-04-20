d464d6525df8cb4648e38d5b33c55989c8e6b442
sujkim <sujkim@datastreams.co.kr>
2017-04-20 12:48:12 +0900

    [fix] builders bin files

---
+++ src/main/resources/binForBuilder/macgyver.bat
@@ -0,0 +1,41 @@
+@echo off
+
+rem // Parameter To Run installer
+set REL_PATH_BIN_TO_HOME=..\
+set REL_PATH_HOME_TO_LIB=.\lib   
+
+rem // Save current directory and change to target directory
+set PATH_TO_BIN_DIR=%~dp0
+pushd %PATH_TO_BIN_DIR%
+pushd %REL_PATH_BIN_TO_HOME%
+set PATH_TO_INSTALLER_HOME=%CD%
+pushd %REL_PATH_HOME_TO_LIB%
+set PATH_TO_LIB_DIR=%CD%
+popd
+popd
+popd
+
+
+:init
+@rem Get command-line arguments, handling Windowz variants
+
+if not "%OS%" == "Windows_NT" goto win9xME_args
+if "%@eval[2+2]" == "4" goto 4NT_args
+
+:win9xME_args
+@rem Slurp the command line arguments.
+set CMD_LINE_ARGS=
+set _SKIP=2
+
+:win9xME_args_slurp
+if "x%~1" == "x" goto execute
+
+set CMD_LINE_ARGS=%*
+goto execute
+
+:4NT_args
+@rem Get arguments from the 4NT Shell from JP Software
+set CMD_LINE_ARGS=%$
+
+:execute
+java -cp "%PATH_TO_LIB_DIR%/*" install.Start -macgyver %CMD_LINE_ARGS%
