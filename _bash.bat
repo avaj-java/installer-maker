20e05f2ec77cdaec2dcf123cf8e023274862d60a
souljungkim <souljungkim@gmail.com>
2017-06-15 12:44:00 +0900

    add bin file to open gitbash for develop

---
+++ _bash.bat
@@ -0,0 +1,13 @@
+:::::::::::::::::::::::::
+::::: x86
+:::::::::::::::::::::::::
+
+:: start "" "%SYSTEMDRIVE%\Program Files (x86)\Git\bin\sh.exe" --login
+
+:::::::::::::::::::::::::
+::::: x64
+:::::::::::::::::::::::::
+
+start "" "%PROGRAMFILES%\Git\bin\sh.exe" --login
+
+exit
