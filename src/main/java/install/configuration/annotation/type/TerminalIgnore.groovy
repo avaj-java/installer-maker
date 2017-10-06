0b0859c58ad477d8608c314da23555f789dd2013
souljungkim <souljungkim@gmail.com>
2017-10-06 22:33:52 +0900

    refs #2 add new annotation for help

---
+++ src/main/java/install/configuration/annotation/type/TerminalIgnore.groovy
@@ -0,0 +1,13 @@
+package install.configuration.annotation.type
+
+import java.lang.annotation.*
+
+/**
+ * Created by sujkim on 2017-06-11.
+ */
+@Inherited
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@interface TerminalIgnore {
+
+}
