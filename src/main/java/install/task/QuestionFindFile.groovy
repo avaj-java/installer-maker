ee4df9474101650e9d26f97a9db8ece308f2f84e
souljungkim <souljungkim@gmail.com>
2018-01-02 02:17:14 +0900

    Update QuestionFindFile (you can add default result list)

--- src/main/java/install/task/QuestionFindFile.groovy
+++ src/main/java/install/task/QuestionFindFile.groovy
@@ -25,6 +25,9 @@ class QuestionFindFile extends TaskUtil{
     @Value(name="find.file.name", required=true)
     String searchFileName
 
+    @Value("find.result.default")
+    List<String> resultDefaultList
+
     @Value("find.result.edit.relpath")
     String editResultPath
 
@@ -57,9 +60,16 @@ class QuestionFindFile extends TaskUtil{
         logger.info ""
 
         Thread threadSearcher = Util.newThread(' <Stoped Searching>      '){
+            int defaultCount = 0
             List<File> foundFileList = FileMan.findAllWithProgressBar(searchRootPath, searchFileName, searchIf) { data ->
                 File foundFile = data.item
-                int count = data.count
+                if (!data.stringList && resultDefaultList){
+                    resultDefaultList.each{
+                        data.stringList << "  ${++defaultCount}) ${it}"
+                        itemList << new File(it)
+                    }
+                }
+                int count = data.count + defaultCount
                 String editedPath = (editResultPath) ? FileMan.getFullPath(foundFile.path, editResultPath) : foundFile.path
                 data.stringList << "  ${count}) ${editedPath}"
                 itemList << new File(editedPath)
